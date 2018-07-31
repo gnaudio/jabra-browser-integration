a937841008baae332b7cc31dd6516cad90089109
Morten Christensen <extmchristensen@jabra.com>
2018-07-31 14:59:00 +0200

    Added configurable logging to extension and client api 1.3

--- src/ChromeWebExtension/content-script.js
+++ src/ChromeWebExtension/content-script.js
@@ -26,13 +26,50 @@ SOFTWARE.
 */
 
 (function () {
+  // Make logLevel variable in sync with storage (updated by options page)
+  // and forward changes to page script using a message as well.
+  var logLevel = 1;
+
+  chrome.storage.local.get('logLevel', function(items) {
+    logLevel = parseInt(items.logLevel || "1");
+    window.postMessage({
+      direction: "jabra-headset-extension-from-content-script",
+      message: "Event: logLevel " + logLevel
+    }, "*");
+    chrome.storage.onChanged.addListener(function(changes, areaName) {
+      for (key in changes) {
+        var storageChange = changes[key];
+        if (key='logLevel' && areaName === 'local' ) {
+          logLevel = storageChange.newValue;
+          window.postMessage({
+            direction: "jabra-headset-extension-from-content-script",
+            message: "Event: logLevel " + logLevel
+          }, "*");
+        }
+      }
+    });
+  });
 
   // From page script
   window.addEventListener("message", function (event) {
     if (event.source === window &&
         event.data.direction &&
         event.data.direction === "jabra-headset-extension-from-page-script") {
-      window.chrome.runtime.sendMessage({ message: event.data.message });
+
+      var msg = event.data.message;
+
+      if (msg === "logLevel") {
+        window.postMessage({
+          direction: "jabra-headset-extension-from-content-script",
+          message: "Event: logLevel " + logLevel
+        }, "*");
+      } else {
+        if (logLevel>=4) { // Log if Loglevel >= Trace
+          console.log("Sending message command to background script: " + msg);
+        }
+
+        window.chrome.runtime.sendMessage({ message: msg });
+      }
     }
   });
 
@@ -40,19 +77,40 @@ SOFTWARE.
   window.chrome.runtime.onMessage.addListener(
     function (request) {
       if (request.message) {
+        if (logLevel>=4) { // Log message if Loglevel >= Trace
+          console.log(request.message);
+        }
         window.postMessage({
             direction: "jabra-headset-extension-from-content-script",
             message: request.message
           },
           "*");
       }
-      if (request.error) {
+      else if (request.error) {
+        if (logLevel>=1) { // Log error if Loglevel >= Error.
+          console.error(request.error);
+        }
+        // Forward error to api to be handled there if needed.
         window.postMessage({
           direction: "jabra-headset-extension-from-content-script",
           error: request.error
         },
           "*");
       }
-    });
-
+      else if (request.info) {
+        if (logLevel>=3) { // Log message if Loglevel >= Info
+          console.log(request.log);
+        }
+      } 
+      else if (request.trace) {
+        if (logLevel>=4) { // Log message if Loglevel >= Trace
+          console.log(request.trace);
+        }
+      }
+      else if (request.warn) {
+        if (logLevel>=2) { // Log message if Loglevel >= Info
+          console.log(request.warn);
+        }
+      }
+  });
 })();
