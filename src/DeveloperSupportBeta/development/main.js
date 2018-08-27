3b72ef00d2d3beecd35ce67b06ff3b2c6c8fec9e
Morten Christensen <extmchristensen@jabra.com>
2018-08-27 12:20:49 +0200

    Moved samples from other repo inside this repo + updates

---
+++ src/DeveloperSupportBeta/development/main.js
@@ -0,0 +1,99 @@
+﻿/// <reference path="../jabra.browser.integration-2.0.d.ts" />
+
+// DOM loaded
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Use the Jabra library
+  jabra.init().then(() => {
+    toastr.info("Jabra library initialized successfully");
+  }).catch((msg) => {
+    // Add nodes to show the error message
+    var div = document.createElement("div");
+    var att = document.createAttribute("class");
+    att.value = "wrapper";
+    div.setAttributeNode(att);
+    div.innerHTML = msg;
+    var br = document.createElement("br");
+    var list = document.getElementById("section");
+    list.insertBefore(br, list.childNodes[0]);
+    list.insertBefore(div, list.childNodes[0]);
+  });
+
+  jabra.addEventListener("mute", (event) => {
+    toastr.info("The device requested to be muted");
+  });
+
+  jabra.addEventListener("unmute", (event) => {
+    toastr.info("The device requested to be unmuted");
+  });
+
+  jabra.addEventListener("device attached", (event) => {
+    toastr.info("A device was attached");
+  });
+
+  jabra.addEventListener("device detached", (event) => {
+    toastr.info("A device was detached");
+  });
+
+  jabra.addEventListener("acceptcall", (event) => {
+    toastr.info("Accept call from the device");
+  });
+
+  jabra.addEventListener("rejectCall", (event) => {
+    toastr.info("Accept call from the device");
+  });
+
+  jabra.addEventListener("endcall", (event) => {
+    toastr.info("End call from the device");
+  });
+
+  jabra.addEventListener("flash", (event) => {
+    toastr.info("Flash from the device");
+  });
+
+  document.getElementById('ring').onclick = function () {
+    jabra.ring();
+  }
+  document.getElementById('offhook').onclick = function () {
+    jabra.offHook();
+  }
+  document.getElementById('onhook').onclick = function () {
+    jabra.onHook();
+  }
+  document.getElementById('mute').onclick = function () {
+    jabra.mute();
+  }
+  document.getElementById('unmute').onclick = function () {
+    jabra.unmute();
+  }
+  document.getElementById('hold').onclick = function () {
+    jabra.hold();
+  }
+  document.getElementById('resume').onclick = function () {
+    jabra.resume();
+  }
+  document.getElementById('getactivedevice').onclick = function () {
+    jabra.getActiveDevice().then(
+      function (id) {
+        alert(id);
+      }
+    );
+  }
+  document.getElementById('getdevices').onclick = function () {
+    jabra.getDevices().then(
+      function(devices) {
+        alert(JSON.stringify(devices));
+      }
+    );
+  }
+  document.getElementById('setactivedevice0').onclick = function () {
+    jabra.setActiveDevice(0);
+  }
+  document.getElementById('setactivedevice1').onclick = function () {
+    jabra.setActiveDevice(1);
+  }
+  document.getElementById('setactivedevice2').onclick = function () {
+    jabra.setActiveDevice(2);
+  }
+
+}, false);
