09b6b0672a69d4cd549aa1971fbcbb918f0a0d9d
Morten Christensen <extmchristensen@jabra.com>
2018-09-25 14:49:12 +0200

    Added local web server utility

---
+++ src/serve.bat
@@ -0,0 +1,4 @@
+REM Start local webserver for manual testing. 
+REM Requires node and "local-web-server" to be installed globally with local certificate as noted in "local-web-server" docs.
+ECHO Open "https://localhost:8000/DeveloperSupportBeta/test/" (or one of the other examples) in browser to start testing
+ws --https
