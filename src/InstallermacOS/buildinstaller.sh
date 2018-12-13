8c12af3bf5c89cba6059e6eee2c2bc4b61b384d5
Rashmi Vyshnavi <rvyshnavi.ext@jabra.com>
2018-12-13 12:21:56 +0530

    Corrected Package Version

--- src/InstallermacOS/buildinstaller.sh
+++ src/InstallermacOS/buildinstaller.sh
@@ -9,5 +9,5 @@ rm -v ${ROOT_FOLDER}/jabrachromehost ${ROOT_FOLDER}/libjabra.dylib
 cp -a ../ChromeHost/bin/jabrachromehost ${ROOT_FOLDER}
 cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ${ROOT_FOLDER}
 pkgbuild --root ROOT  --identifier com.jabra.chromehost --version 2.0 JabraChromeHost2.0.unsigned.pkg
-productsign --sign "Developer ID Installer: GN Audio AS" JabraChromeHost0.5.unsigned.pkg JabraChromeHost0.5.pkg
-hdiutil create -volname JabraChromeHost -srcfolder ./JabraChromeHost0.5.pkg -ov -format UDZO JabraChromeHost0.5.dmg
+productsign --sign "Developer ID Installer: GN Audio AS" JabraChromeHost2.0.unsigned.pkg JabraChromeHost2.0.pkg
+hdiutil create -volname JabraChromeHost -srcfolder ./JabraChromeHost2.0.pkg -ov -format UDZO JabraChromeHost2.0.dmg
