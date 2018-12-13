34ca8bd991f09bec3eb36c768525b94885b6ce1a
Rashmi Vyshnavi <rvyshnavi.ext@jabra.com>
2018-12-13 11:05:00 +0530

    Updated libjabra libraries (v1.7.x). Also fixed build issues.

--- src/InstallermacOS/buildinstaller.sh
+++ src/InstallermacOS/buildinstaller.sh
@@ -4,9 +4,10 @@ cd "${PWD}/../ChromeHost"
 cmake -DCMAKE_BUILD_TYPE=Release .
 make
 cd -
-rm -v ${PWD}/ROOT/Library/Google/Chrome/NativeMessagingHosts/jabrachromehost ${PWD}/ROOT/Library/Google/Chrome/NativeMessagingHosts/libjabra.dylib
-cp -a ../ChromeHost/bin/jabrachromehost ./ROOT/Library/Google/Chrome/NativeMessagingHosts/
-cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ./ROOT/Library/Google/Chrome/NativeMessagingHosts
+ROOT_FOLDER=${PWD}/ROOT/Library/Google/Chrome/NativeMessagingHosts
+rm -v ${ROOT_FOLDER}/jabrachromehost ${ROOT_FOLDER}/libjabra.dylib
+cp -a ../ChromeHost/bin/jabrachromehost ${ROOT_FOLDER}
+cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ${ROOT_FOLDER}
 pkgbuild --root ROOT  --identifier com.jabra.chromehost --version 0.5 JabraChromeHost0.5.unsigned.pkg
 productsign --sign "Developer ID Installer: GN Audio AS" JabraChromeHost0.5.unsigned.pkg JabraChromeHost0.5.pkg
 hdiutil create -volname JabraChromeHost -srcfolder ./JabraChromeHost0.5.pkg -ov -format UDZO JabraChromeHost0.5.dmg
