5e9e6d2e3684f5b83efaa0cace6128b038a74ac9
Rashmi Vyshnavi <rvyshnavi.ext@jabra.com>
2018-12-13 11:35:32 +0530

    Fixed compiler issue & Updated version to 2.0

--- src/InstallermacOS/buildinstaller.sh
+++ src/InstallermacOS/buildinstaller.sh
@@ -8,6 +8,6 @@ ROOT_FOLDER=${PWD}/ROOT/Library/Google/Chrome/NativeMessagingHosts
 rm -v ${ROOT_FOLDER}/jabrachromehost ${ROOT_FOLDER}/libjabra.dylib
 cp -a ../ChromeHost/bin/jabrachromehost ${ROOT_FOLDER}
 cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ${ROOT_FOLDER}
-pkgbuild --root ROOT  --identifier com.jabra.chromehost --version 0.5 JabraChromeHost0.5.unsigned.pkg
+pkgbuild --root ROOT  --identifier com.jabra.chromehost --version 2.0 JabraChromeHost2.0.unsigned.pkg
 productsign --sign "Developer ID Installer: GN Audio AS" JabraChromeHost0.5.unsigned.pkg JabraChromeHost0.5.pkg
 hdiutil create -volname JabraChromeHost -srcfolder ./JabraChromeHost0.5.pkg -ov -format UDZO JabraChromeHost0.5.dmg
