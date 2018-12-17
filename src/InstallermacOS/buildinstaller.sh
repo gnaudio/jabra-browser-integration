491af94d5de3e6de1f8305f34f1439550b020a3d
Rashmi Vyshnavi <rvyshnavi.ext@jabra.com>
2018-12-17 11:02:48 +0530

    Fixed Installation issue.

--- src/InstallermacOS/buildinstaller.sh
+++ src/InstallermacOS/buildinstaller.sh
@@ -8,6 +8,6 @@ ROOT_FOLDER=${PWD}/ROOT/Library/Google/Chrome/NativeMessagingHosts
 rm -v ${ROOT_FOLDER}/jabrachromehost ${ROOT_FOLDER}/libjabra.dylib
 cp -a ../ChromeHost/bin/jabrachromehost ${ROOT_FOLDER}
 cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ${ROOT_FOLDER}
-pkgbuild --root ROOT  --identifier com.jabra.chromehost --version 2.0 JabraChromeHost2.0.unsigned.pkg
+pkgbuild --root "ROOT" --scripts ./scripts --identifier com.jabra.chromehost --version 2.0 JabraChromeHost2.0.unsigned.pkg
 productsign --sign "Developer ID Installer: GN Audio AS" JabraChromeHost2.0.unsigned.pkg JabraChromeHost2.0.pkg
 hdiutil create -volname JabraChromeHost -srcfolder ./JabraChromeHost2.0.pkg -ov -format UDZO JabraChromeHost2.0.dmg
