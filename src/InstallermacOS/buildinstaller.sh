becd9a50921d7769ab9b3f7f97bcdb0d97932f23
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2019-01-08 16:16:42 +0100

    Update buildinstaller.sh

--- src/InstallermacOS/buildinstaller.sh
+++ src/InstallermacOS/buildinstaller.sh
@@ -11,9 +11,9 @@ ROOT_FOLDER=${PWD}/ROOT/Library/Google/Chrome/NativeMessagingHosts
 cp -a ../ChromeHost/bin/jabrachromehost ./ROOT/Library/Google/Chrome/NativeMessagingHosts
 cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ./ROOT/Library/Google/Chrome/NativeMessagingHosts
 
-#Uncomment to sign the application. Provide a valid signing certificate.
-#codesign --sign "Developer ID Application: GN Audio AS" ${ROOT_FOLDER}/jabrachromehost
-pkgbuild --root "ROOT" --scripts ./scripts --identifier com.jabra.chromehost --version 2.0 JabraChromeHost2.0.unsigned.pkg
+# Sign the application. Provide a valid signing certificate.
+codesign --sign "Developer ID Application: GN Audio AS" ./ROOT/Library/Google/Chrome/NativeMessagingHosts/jabrachromehost
+#pkgbuild --root "ROOT" --scripts ./scripts --identifier com.jabra.chromehost --version 2.0 JabraChromeHost2.0.unsigned.pkg
 #Uncomment to sign the package and create dmg file. Provide a valid signing certificate.
 #productsign --sign "Developer ID Installer: GN Audio AS" JabraChromeHost2.0.unsigned.pkg JabraChromeHost2.0.pkg
 #hdiutil create -volname JabraChromeHost -srcfolder ./JabraChromeHost2.0.pkg -ov -format UDZO JabraChromeHost2.0.dmg
