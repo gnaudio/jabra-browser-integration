228789b0112dfc2188bf993e67b181a8903075e5
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2019-01-08 15:58:03 +0100

    Update buildinstaller.sh

--- src/InstallermacOS/buildinstaller.sh
+++ src/InstallermacOS/buildinstaller.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-cd "${PWD}/../ChromeHost"
+cd "./src/ChromeHost"
 cmake -DCMAKE_BUILD_TYPE=Release .
 make
-cd -
+cd ../InstallermacOS
+rm -v ./ROOT/Library/Google/Chrome/NativeMessagingHosts/jabrachromehost ./ROOT/Library/Google/Chrome/NativeMessagingHosts/libjabra.dylib
+
 ROOT_FOLDER=${PWD}/ROOT/Library/Google/Chrome/NativeMessagingHosts
-rm -v ${ROOT_FOLDER}/jabrachromehost ${ROOT_FOLDER}/libjabra.dylib
-cp -a ../ChromeHost/bin/jabrachromehost ${ROOT_FOLDER}
-cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ${ROOT_FOLDER}
+
+cp -a ../ChromeHost/bin/jabrachromehost ./ROOT/Library/Google/Chrome/NativeMessagingHosts
+cp -a ../ChromeHost/SDKmacOS/libjabra.dylib ./ROOT/Library/Google/Chrome/NativeMessagingHosts
+
 #Uncomment to sign the application. Provide a valid signing certificate.
 #codesign --sign "Developer ID Application: GN Audio AS" ${ROOT_FOLDER}/jabrachromehost
 pkgbuild --root "ROOT" --scripts ./scripts --identifier com.jabra.chromehost --version 2.0 JabraChromeHost2.0.unsigned.pkg
