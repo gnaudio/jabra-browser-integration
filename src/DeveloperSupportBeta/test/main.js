c70ef8f20ceac6ab80bb18aadea1140636941cf7
Rasmus Noerskov <rasmus@norskov.org>
2019-09-19 11:11:35 +0200

    Update examples to use v3.0

--- src/DeveloperSupportBeta/test/main.js
+++ src/DeveloperSupportBeta/test/main.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="../../JavaScriptLibrary/jabra.browser.integration-2.0.d.ts" />
+﻿/// <reference path="../../JavaScriptLibrary/jabra.browser.integration-3.0.d.ts" />
 
 // DOM loaded
 document.addEventListener('DOMContentLoaded', function () {
