678c6399cb4bef66eea4ef953f349438a2fb0e8a
Rasmus Noerskov <rasmus@norskov.org>
2019-09-19 11:01:24 +0200

    Update version numberr

--- src/JavaScriptLibrary/src/core.ts
+++ src/JavaScriptLibrary/src/core.ts
@@ -28,7 +28,7 @@ SOFTWARE.
 /**
  * Version of this javascript api (should match version number in file apart from possible alfa/beta designator).
  */
-export const apiVersion = "2.1.0.beta1";
+export const apiVersion = "3.0.0";
 
 /**
  * Is the current version a beta ?
